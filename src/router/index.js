13dfc9c7c77b4848e2c16b67d8e13d377d627b58
giulliano.ortiz <giulliano.ortiz@unna.com.pe>
2023-08-01 18:22:55 -0500

    set to history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Player from '../views/Player.vue'
 import Login from '../views/Login.vue'
 import loginService from '../services/login.service'
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
